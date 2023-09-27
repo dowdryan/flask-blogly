a0036619edd63a745c7ad25d3b5444d29dd3fb30
Ryan <dowdryan8@gmail.com>
2023-09-27 10:28:47 -0400

    Added Part 2

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 """Blogly application."""
 # Set up virtual environment
-
-from flask import Flask, render_template, request, redirect, url_for
-from models import db, connect_db, User #, Post
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask_blogly'
@@ -16,17 +15,18 @@ connect_db(app)
 db.create_all()
 
 # =======================================================================
+# Users Route
 @app.route("/")
 def homepage():
     tablename=User.__tablename__
     users=User.query.order_by(User.first_name, User.last_name).all()
-    return render_template("homepage.html",
+    return render_template("users/homepage.html",
                            tablename=tablename,
                            users=users)
 
 @app.route("/users/new", methods=["GET"])
 def add_user():
-    return render_template("createuser.html")
+    return render_template("users/createuser.html")
 
 @app.route("/users/new", methods=["POST"])
 def create_user():
@@ -38,17 +38,16 @@ def create_user():
     db.session.commit()
     return redirect("/")
 
-# Need to create a route that will go to the specific users profile.
 @app.route("/users/<int:user_id>")
 def user_profile(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template("userprofile.html", 
+    return render_template("users/userprofile.html", 
                            user=user)
 
 @app.route("/users/<int:user_id>/edit")
 def edit_user(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template("edituser.html", 
+    return render_template("users/edituser.html", 
                            user=user)
 
 @app.route("/users/<int:user_id>/edit", methods=["POST"])
@@ -67,6 +66,56 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()
     return redirect("/")
+
+# =======================================================================
+# Posts Route
+@app.route("/users/<int:user_id>/posts/new")
+def add_post(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("posts/createpost.html",
+                           user=user)
+
+@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+def create_post(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    user_id=user.id)
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Post '{new_post.title}' added.")
+    return redirect(f"/users/{user_id}")
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/showpost.html',
+                           post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/editpost.html', 
+                           post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+    return redirect(f"/users/{post.user_id}")
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post '{post.title} deleted.")
+    return redirect(f"/users/{post.user_id}")
 
 # # =============================================================================
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models for Blogly."""
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 default_img = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
@@ -21,7 +22,25 @@ class User(db.Model):
     @property
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
-
+    
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer,
+                 primary_key=True,
+                 autoincrement=True)
+    title = db.Column(db.String(100),
+                      nullable=False)
+    content = db.Column(db.Text,
+                        nullable=False)
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable=False)
+    @property
+    def friendly_date(self):
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 def connect_db(app):
     db.app = app
